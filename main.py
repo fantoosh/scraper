acee8625ec7bb305b3ad72f508cc64cdaec0c2a6
fantoosh <avalon74@protonmail.com>
2022-06-09 17:44:09 +0100

    Edited Readme.md file

---
+++ main.py
@@ -0,0 +1,14 @@
+#  Using the find_all method for bs objects we can get a list of tags, not just the first occurence
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+html = urlopen('https://www.linkedin.com')
+
+bs = BeautifulSoup(html.read(), 'html.parser')
+
+name_list = bs.findAll('span', {'class': 'green'})
+for name in name_list:
+    print(name.get_text())
+
+new = bs.attrs
+print(bs.body)
